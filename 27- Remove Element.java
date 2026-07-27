d08dc00a85dfb35ce0bfc86c204c6fedb9ee4d89
Sagar Jadhav <sagarjadhav281205@gmail.com>
2026-07-28 00:05:15 +0530

    Update 27- Remove Element.java

--- 27- Remove Element.java
+++ 27- Remove Element.java
@@ -1,1 +1,17 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0)
+            return 0;
 
+        int ptr = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[ptr] = nums[i];
+                ptr++;
+            }
+        }
+
+        return ptr;
+    }
+}
